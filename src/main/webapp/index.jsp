e0842b36e1cddeff1b1eccc33a0ba31011340c35
Mounik vamshi <Mounik vamshi@Mounika-Vamshi.connectify>
2016-10-14 17:04:46 +0530

    Maven project

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body bgcolor="red">
+<title>Login Page</title>
+<h2>Hello World!</h2>
+UserName<input type="text" name="username"><br>
+Password<input type="password" name="password"><br>
+<input type="submit" value="login">
+
+</body>
+</html>
---
+++ src/test/java/shoppingcart/Test.java
@@ -0,0 +1,18 @@
+package shoppingcart;
+
+import com.model.Product;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Product p=new Product();
+		p.setId(100);
+		p.setName("xyz");
+		System.out.println(p.getId());
+		System.out.println(p.getName());
+	}
+
+}
